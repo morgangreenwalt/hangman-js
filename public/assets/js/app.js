8739b06471aa94b55553fef6b57f1842911dff17
morgan greenwalt <you@example.com>
2017-08-20 11:21:16 -0500

    html

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
 
     // Variables
-    var lives = 12;
-    var lettersArray = [];
+    var lives;
+    var lettersArray;
     var correctGuess;
     var wins = 0;
     var words = ["country", "pop", "jazz", "rap", "dance", "electronic"];
     var randomSelect;
 
     function startAndReset() {
+        lives = 12;
+        lettersArray = [];
         var randomSelect = Math.floor((Math.random() * words.length));
         selectedWord = words[randomSelect];
         console.log(selectedWord);
@@ -19,9 +21,18 @@ $(document).ready(function() {
             blanks[i] = "_";
         }
         console.log(blanks);
-    }
+    };
+
+    function addHTML() {
+        var letters = document.getElementById("letters-guessed");
+        letters.innerHTML = lettersArray;
+
+        var life = document.getElementById("lives");
+        life.innerHTML = lives;
+    };
 
     startAndReset();
+    addHTML();
 
     // Function taking user's guess
     function userGuesses() {
@@ -37,29 +48,33 @@ $(document).ready(function() {
 
             if (checkLetter === -1) {
                 lettersArray.push(userChoice);
+                addHTML();
                 console.log(lettersArray);
+
 
             } else {
                 for (var i = 0; i < selectedWord.length; i++) {
                     blanks[checkLetter] = userChoice;
                     blanks[selectedWord.lastIndexOf(userChoice)] = userChoice;
+
                 }
                 console.log(blanks);
             }
         };
     };
+
     userGuesses();
 
     function gameOver() {
         if (lives === -1) {
             alert("No more guesses remaining. Try again!");
             startAndReset();
-            lives = 12;
+            addHTML();
         } else if (blanks.includes("_") === false) {
             alert("You win! Come back for more.");
             startAndReset();
-            lives = 12;
+            addHTML();
         }
-
     }
+
 });
