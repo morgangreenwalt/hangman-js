6825163e3ec4e1f2dc7e037db1df038a79efe277
morgan greenwalt <you@example.com>
2017-08-20 10:49:59 -0500

    final js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,29 +1,65 @@
-// Variables
-var guessessRemain = 10;
-var lettersGuessed = [];
-var wins = 0;
-var words = ["country", "pop", "jazz", "rap", "dance", "electronic"];
-var randomSelect = Math.floor((Math.random() * words.length));
-console.log(randomSelect);
+$(document).ready(function() {
 
-// Choose a random word
-var selectedWord = words[randomSelect];
-console.log(selectedWord);
+    // Variables
+    var lives = 10;
+    var lettersArray = [];
+    var correctGuess;
+    var wins = 0;
+    var words = ["country", "pop", "jazz", "rap", "dance", "electronic"];
+    var randomSelect;
 
-// Have the word appear in _ _ _
-var emptyWord = selectedWord.length;
-console.log(emptyWord);
-var blanks = selectedWord.split("").map(function() { return "_" });
-console.log(blanks);
+    function startAndReset() {
+        var randomSelect = Math.floor((Math.random() * words.length));
+        selectedWord = words[randomSelect];
+        console.log(selectedWord);
 
-// Function taking user's guess
-function userGuesses() {
-    document.onkeyup = function(event) {
-        var userChoice = event.key;
-        guessessRemain--;
-        console.log(userChoice);
-        console.log(guessessRemain);
+        // Have the word appear in "_ _ _"
+        blanks = [];
+        for (var i = 0; i < selectedWord.length; i++) {
+            blanks[i] = "_";
+        }
+        console.log(blanks);
+    }
+
+    startAndReset();
+
+    // Function taking user's guess
+    function userGuesses() {
+        document.onkeyup = function(event) {
+            var userChoice = event.key;
+            lives--;
+            gameOver();
+            console.log(userChoice);
+            console.log(lives);
+
+            var checkLetter = selectedWord.indexOf(userChoice);
+            console.log(checkLetter);
+
+            if (checkLetter === -1) {
+                lettersArray.push(userChoice);
+                console.log(lettersArray);
+
+            } else {
+                for (var i = 0; i < selectedWord.length; i++) {
+                    blanks[checkLetter] = userChoice;
+                    blanks[selectedWord.lastIndexOf(userChoice)] = userChoice;
+                }
+                console.log(blanks);
+            }
+        };
     };
-};
+    userGuesses();
 
-userGuesses();
+    function gameOver() {
+        if (lives === -1) {
+            alert("No more guesses remaining. Try again!");
+            startAndReset();
+            lives = 10;
+        } else if (blanks.includes("_") === false) {
+            alert("You win! Come back for more.");
+            startAndReset();
+            lives = 10;
+        }
+
+    }
+});
